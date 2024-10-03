036229a6010ed5d5a788d20feafed0724255f014
emanuel <emanuelkguimaraes@gamil.com>
2024-10-03 08:59:24 -0300

    this is elon musk

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+    console.log("Connected with DB");
+  } catch (error) {
+    console.log(error);
+  }
+})();import mongoose from "mongoose";
+
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+    console.log("Connected with DB");
+  } catch (error) {
+    console.log(error);
+  }
+})();
